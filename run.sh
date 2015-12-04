eb91fd1a5ed4adce910c583cb77b5195e8a40d7f
gtt116 <gtt116@gmail.com>
2015-12-04 16:51:30 +0800

    copy to var/www

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ if [ -d "$HERE/.venv" ]; then
     $HERE/.venv/bin/python $HERE/main.py
 else
     virtualenv $HERE/.venv
-    $HERE/.venv/bin/pip install -r requirements.txt
+    $HERE/.venv/bin/pip install -r $HERE/requirements.txt
 fi
+
+cp -v $HERE/index.html /var/www/tieba/zhaopin
+cp -v $HERE/data.json /var/www/tieba/zhaopin
