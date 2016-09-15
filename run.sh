d73e4db6e4d2eceda2161088961887e92bb8d8f7
gtt116 <gtt116@gmail.com>
2016-09-15 15:58:00 +0800

    calling wrapper.py in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 HERE=`dirname $0`
 
 if [ -d "$HERE/.venv" ]; then
-    $HERE/.venv/bin/python $HERE/main.py
+    $HERE/.venv/bin/python $HERE/wrapper.py
 else
     virtualenv $HERE/.venv
     $HERE/.venv/bin/pip install -r $HERE/requirements.txt
